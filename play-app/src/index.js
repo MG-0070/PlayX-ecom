221fca3ab21ae04d1b459144ee0aa3a217d615c3
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2022-04-03 17:17:43 +0530

    remove commented code

--- play-app/src/index.js
+++ play-app/src/index.js
@@ -1,22 +1,4 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import { makeServer } from "./server";
 
-// import { BrowserRouter } from "react-router-dom";
-
-// // Call make Server
-// makeServer();
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//     <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
