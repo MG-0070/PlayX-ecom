c9d4c70735d2f7605c2482f42b4042c30f95a0dc
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2022-04-18 04:03:47 +0530

    add styles: login and signup css

--- play-app/src/Pages/Auth/Login/Login.jsx
+++ play-app/src/Pages/Auth/Login/Login.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import "./Login.css";
+import { Link } from 'react-router-dom';
+import {FaChevronRight} from "react-icons/fa";
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex flex-center mt-l'>
+       <form className="login-form  pt-l">
+            <div className="f-m font-xxl mb-s">Login</div>
+            <div className=" mt-l ">
+                <label  className="login-email-label font-s">Email Address</label>
+                <input type="text" className="f-s mt-s " name="email" id="email" placeholder="Enter your email" />
+            </div>
+            <div className="mt-l">
+                <label  className="login-email-label font-s">Password</label>
+                <input type="text" className="f-s  mt-s " name="email" id="email" placeholder="Enter your password" />
+            </div>
+            <div className="mt-l flex">
+                <input type="checkbox" id="remember" name="remember" className="login-checkbox" />
+                <label  className="font-xs f-s">Remember me</label>
+                <h3 className="login-text pl-s font-xs">Forgot your password?</h3>
+            </div>
+            <div className="mt-l">
+                <button className="login-btn f-s">Login</button>
+            </div>
+            <div className="mt-l mb-l">
+               <Link to="/signup" className="f-m">Create New Account<FaChevronRight className='pl-s pt-s'/></Link>
+            </div>
+        </form>
+
+    </div>
   )
 }
 
--- play-app/src/Pages/Auth/Signup/Signup.jsx
+++ play-app/src/Pages/Auth/Signup/Signup.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import "./Signup.css";
+import {FaChevronRight} from "react-icons/fa";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='flex flex-center mt-l'>
+       <form className="login-form  pt-l">
+            <div className="f-m font-xxl mb-s">Signup</div>
+            <div className=" mt-l ">
+                <label  className="login-email-label font-s">Email Address</label>
+                <input type="text" className="f-s mt-s " name="email" id="email" placeholder="Enter your email" />
+            </div>
+            <div className="mt-l">
+                <label  className="login-email-label font-s">Password</label>
+                <input type="text" className="f-s  mt-s " name="email" id="email" placeholder="Enter your password" />
+            </div>
+            <div className="mt-l flex">
+                <input type="checkbox" id="remember" name="remember" className="login-checkbox" />
+                <label for="remember" className="font-xs f-s">I accept all term & conditions</label>
+            </div>
+            <div className="mt-l">
+                <button className="login-btn f-s">Create New Account</button>
+            </div>
+            <div className="mt-l mb-l">
+                <Link to='/login' className='f-m'>Already have an account<FaChevronRight className='pl-s pt-s f-m'/></Link>
+            </div>
+        </form>
+    </div>
   )
 }
 
